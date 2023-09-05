213d34cf20041ef784b511e9aa974c9569345cbc
Melanie Wilson <melaniemwilson1@gmail.com>
2023-09-05 17:52:49 -0400

    completed lab:

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,15 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ''.join(items)
+    
+    if len(items) == 2:
+        return ' and '.join(items)
+    
+    if len(items) > 2:
+        i=0
+        string = ""
+        while i < (len(items)-1):
+            string += items[i] + ", "
+            i += 1
+        string += "and " + items[i]
+        return string
